4cc14ec7f53afd9c22c88d7d53058188b8f8f8c8
Nick Charlton <nick@nickcharlton.net>
2015-12-22 11:41:03 +0000

    Initial commit; creates Xcode project shell.

---
+++ ShareBlob/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ShareBlob
+//
+//  Created by Nick Charlton on 22/12/2015.
+//  Copyright © 2015 Nick Charlton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
