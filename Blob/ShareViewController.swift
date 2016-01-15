4f82baf17ff6362e136ee478233790802b332c67
Nick Charlton <nick@nickcharlton.net>
2016-01-15 16:26:44 +0000

    Use 2 spaces for indentation, not 4.

--- Blob/ShareViewController.swift
+++ Blob/ShareViewController.swift
@@ -11,60 +11,60 @@ import Social
 import MobileCoreServices
 
 class ShareViewController: SLComposeServiceViewController {
-    var operationQueue: dispatch_queue_t = dispatch_queue_create("operationQueue", nil)
+  var operationQueue: dispatch_queue_t = dispatch_queue_create("operationQueue", nil)
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
 
-        dispatch_async(operationQueue) {
-            var grabbedUrl = ""
+    dispatch_async(operationQueue) {
+      var grabbedUrl = ""
 
-            while grabbedUrl.isEmpty {
-                self.fetchURL({ url in
-                    grabbedUrl = url.absoluteString
-                })
-            }
+      while grabbedUrl.isEmpty {
+        self.fetchURL({ url in
+          grabbedUrl = url.absoluteString
+        })
+      }
 
-            print(grabbedUrl)
-        }
+      print(grabbedUrl)
+    }
+  }
+
+  override func isContentValid() -> Bool {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return true
+  }
+
+  override func didSelectPost() {
+    // this is the content written in the text box
+    print("contentText: \(contentText)")
+
+    // now pull out the url
+    fetchURL { url in
+      print(url)
     }
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+  }
+
+  override func configurationItems() -> [AnyObject]! {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return []
+  }
+
+  func fetchURL(completionHandler: (url: NSURL) -> Void) {
+    let extensionItems = extensionContext?.inputItems
+    if extensionItems?.count > 0 {
+      let item  = extensionItems![0] as! NSExtensionItem
+      let attachments = item.attachments
+
+      if let urlProvider = attachments![0] as? NSItemProvider {
+        urlProvider.loadItemForTypeIdentifier("public.url", options: nil, completionHandler: { result, error in
+          if let url = result as? NSURL {
+            completionHandler(url: url)
+          }
+        })
+      }
     }
-
-    override func didSelectPost() {
-        // this is the content written in the text box
-        print("contentText: \(contentText)")
-
-        // now pull out the url
-        fetchURL { url in
-            print(url)
-        }
-
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
-    }
-
-    override func configurationItems() -> [AnyObject]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
-
-    func fetchURL(completionHandler: (url: NSURL) -> Void) {
-        let extensionItems = extensionContext?.inputItems
-        if extensionItems?.count > 0 {
-            let item  = extensionItems![0] as! NSExtensionItem
-            let attachments = item.attachments
-
-            if let urlProvider = attachments![0] as? NSItemProvider {
-                urlProvider.loadItemForTypeIdentifier("public.url", options: nil, completionHandler: { result, error in
-                    if let url = result as? NSURL {
-                        completionHandler(url: url)
-                    }
-                })
-            }
-        }
-    }
+  }
 }
